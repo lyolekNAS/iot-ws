3fb79c15f0cfd10f223ad5f96b218af52d591372
lyolek <lyolek@gmail.com>
2025-10-08 17:08:06 +0300

    init

---
+++ src/main/java/org/sav/fornas/iot_ws/IotWsApplication.java
@@ -0,0 +1,13 @@
+package org.sav.fornas.iot_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sav/fornas/iot_ws/IotWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sav.fornas.iot_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
