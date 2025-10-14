adc562edcc86c1503c38633b003251af0d6f78ab
lyolek <lyolek@gmail.com>
2025-10-14 18:54:58 +0300

    виправив тести

--- src/test/java/org/sav/fornas/iotws/IotWsApplicationTests.java
+++ src/test/java/org/sav/fornas/iotws/IotWsApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sav.fornas.iot_ws;
+package org.sav.fornas.iotws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,7 @@ class IotWsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//чи завантажиться?
 	}
 
 }
