2004340eeafeffffefa0dbf8a78e92d62b574d51
Lyolek <lyolek@gmail.com>
2026-08-02 13:53:15 +0300

    Виніс VAULT_URL в конфіги

--- src/test/java/org/sav/fornas/iotws/IotWsApplicationTests.java
+++ src/test/java/org/sav/fornas/iotws/IotWsApplicationTests.java
@@ -2,8 +2,10 @@ package org.sav.fornas.iotws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IotWsApplicationTests {
 
 	@Test
